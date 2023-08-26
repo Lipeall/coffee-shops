5f40f461973930a098ebe1d72e082cf615467dac
Filipe <filipe-all@hotmail.com>
2023-08-26 10:02:44 -0300

    first commit

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// Definindo o tipo para o componente
+const Home: React.FC = () => {
+  return (
+    <div>
+      <h1>Hello World!</h1>
+    </div>
+  );
+}
+
+export default Home;
