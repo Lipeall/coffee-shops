bcf09b42e6cb8931edc7d39524d1e4acd5e4c9ac
Filipe <filipe-all@hotmail.com>
2023-08-27 11:52:28 -0300

    Implemented responsive design

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,21 +1,63 @@
-import React from 'react';
-import "./Header.scss";
-import { HeaderProps } from "./types";
+import React from "react";
 
-const Header: React.FC<HeaderProps> = ( { titleHeader, navItems } ) => {
-    
-  return (
-    <header>
-        <a className="brand" href={titleHeader.url}>{titleHeader.title}</a>
-        <nav className="navigation">
-            <div className="navigation-items">
-                {navItems.map((item, index) => (
-                    <a key={index} href={item.url}>{item.title}</a>
-                ))}
-            </div>
-        </nav>
-    </header>
-  );
-}
+import { HeaderProps } from "./types";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import "./Header.scss";
+
+const Header: React.FC<HeaderProps> = ({ titleHeader, navItems }) => {
+    return (
+        <Navbar collapseOnSelect expand="lg">
+            <Container>
+				<div>
+						<Navbar.Brand href={titleHeader.url}>
+							{titleHeader.title}
+						</Navbar.Brand>
+						<Navbar.Toggle aria-controls="responsive-navbar-nav" />
+				</div>
+					<div>
+						<Navbar.Collapse
+							id="responsive-navbar-nav"
+							className="navigation-items"
+						>
+							<Nav className="me-auto">
+								{navItems.map((item, index) => (
+									<Nav.Link key={index} href={item.url}>
+										{item.title}
+									</Nav.Link>
+								))}
+							</Nav>
+						</Navbar.Collapse>
+					</div>
+            </Container>
+        </Navbar>
+    );
+};
 
 export default Header;
+// import React from 'react';
+// import "./Header.scss";
+// import { HeaderProps } from "./types";
+// import {
+// 	Container,
+// 	Col,
+// 	Nav,
+// 	Navbar,
+//  } from 'react-bootstrap';
+
+// const Header: React.FC<HeaderProps> = ( { titleHeader, navItems } ) => {
+
+//   return (
+//     <Navbar>
+//       <Container fluid>
+//         	<Navbar.Brand href="#home">{titleHeader.title}</Navbar.Brand>
+// 			<Nav className="navigation-items">
+// 				{navItems.map((item) => (
+// 					<Nav.Link href={item.url}>{item.title}</Nav.Link>
+// 				))}
+// 			</Nav>
+//       </Container>
+//     </Navbar>
+//   );
+// }
+
+// export default Header;
