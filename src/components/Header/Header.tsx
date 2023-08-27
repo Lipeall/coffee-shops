1d4da2fec01ecefea35bf95b5f0fe8f76c952c71
Filipe <filipe-all@hotmail.com>
2023-08-27 13:40:10 -0300

    Created home page

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -8,56 +8,29 @@ const Header: React.FC<HeaderProps> = ({ titleHeader, navItems }) => {
     return (
         <Navbar collapseOnSelect expand="lg">
             <Container>
-				<div>
-						<Navbar.Brand href={titleHeader.url}>
-							{titleHeader.title}
-						</Navbar.Brand>
-						<Navbar.Toggle aria-controls="responsive-navbar-nav" />
-				</div>
-					<div>
-						<Navbar.Collapse
-							id="responsive-navbar-nav"
-							className="navigation-items"
-						>
-							<Nav className="me-auto">
-								{navItems.map((item, index) => (
-									<Nav.Link key={index} href={item.url}>
-										{item.title}
-									</Nav.Link>
-								))}
-							</Nav>
-						</Navbar.Collapse>
-					</div>
+                <div>
+                    <Navbar.Brand href={titleHeader.url}>
+                        {titleHeader.title}
+                    </Navbar.Brand>
+                    <Navbar.Toggle className="navbar-dark" aria-controls="responsive-navbar-nav" />
+                </div>
+                <div>
+                    <Navbar.Collapse
+                        id="responsive-navbar-nav"
+                        className="navigation-items"
+                    >
+                        <Nav className="me-auto">
+                            {navItems.map((item, index) => (
+                                <Nav.Link key={index} href={item.url}>
+                                    {item.title}
+                                </Nav.Link>
+                            ))}
+                        </Nav>
+                    </Navbar.Collapse>
+                </div>
             </Container>
         </Navbar>
     );
 };
 
 export default Header;
-// import React from 'react';
-// import "./Header.scss";
-// import { HeaderProps } from "./types";
-// import {
-// 	Container,
-// 	Col,
-// 	Nav,
-// 	Navbar,
-//  } from 'react-bootstrap';
-
-// const Header: React.FC<HeaderProps> = ( { titleHeader, navItems } ) => {
-
-//   return (
-//     <Navbar>
-//       <Container fluid>
-//         	<Navbar.Brand href="#home">{titleHeader.title}</Navbar.Brand>
-// 			<Nav className="navigation-items">
-// 				{navItems.map((item) => (
-// 					<Nav.Link href={item.url}>{item.title}</Nav.Link>
-// 				))}
-// 			</Nav>
-//       </Container>
-//     </Navbar>
-//   );
-// }
-
-// export default Header;
